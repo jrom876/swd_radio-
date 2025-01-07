182a7aa6b7f0bb6dad377474d5cf73730fb94453
jrom876 <jrom876@gmail.com>
2025-01-07 15:25:35 -0700

    Cleanup antenna.hpp

--- antenna/antenna.hpp
+++ antenna/antenna.hpp
@@ -6,7 +6,6 @@
 	Copyright (C) 2019, 2021 
 	Jacob Romero, Creative Engineering Solutions, LLC
 	cesllc876@gmail.com
-	admin@jrom.io 
 
 	This program is free software; you can redistribute it
 	and/or modify it under the terms of the GNU General Public  
@@ -30,37 +29,37 @@ class ANTENNA {
 	//~ private:
     public:            
         enum ANT_POL {
-			horiz,	// horizontal
-			vert,	// vertical
-			rhcirc,	// right hand circular
-			lhcirc	// left hand circular
-		};
+		horiz,	// horizontal
+		vert,	// vertical
+		rhcirc,	// right hand circular
+		lhcirc	// left hand circular
+	};
 
-		enum ANT_TYPE {
-			mono,	// 1/4 wavelength dipole
-			dipole,	// 1/2 wavelength dipole
-			patch,	// PCB patch
-			horn,	// microwave horn
-			spiral,	// spiral, frequency independent
-			logpt, 	// log periodic tooth, frequency independant
-			parab,	// parabolic
-			arr		// phased-array
-		};	
+	enum ANT_TYPE {
+		mono,	// 1/4 wavelength dipole
+		dipole,	// 1/2 wavelength dipole
+		patch,	// PCB patch
+		horn,	// microwave horn
+		spiral,	// spiral, frequency independent
+		logpt, 	// log periodic tooth, frequency independant
+		parab,	// parabolic
+		arr	// phased-array
+	};	
   
-		ANTENNA();
-		ANTENNA(enum ANT_POL pol,enum ANT_TYPE type,float gain,float freq,float eff,float bw,float area,float zin);
-		
-		enum ANT_POL	pol; 	// Antenna Polarization
-		enum ANT_TYPE	type;	// Antenna Type
-		float 			gain; 	// Gain of RX Antenna in dB
-		float 			freq;	// Center Frequency in MHz
-		float 			eff;	// Antenna Efficiency in %
-		float 			bw;		// Antenna Bandwidth in GHz
-		float 			area;	// Effective Aperature Area	
-		float 			zin;	// Impedance in Ohms
-		
-		float antGainFromFreqGHz(float efficiency, float aperature_area, float freqGHz);
-		float calcAeff(float freqGHz);
+	ANTENNA();
+	ANTENNA(enum ANT_POL pol,enum ANT_TYPE type,float gain,float freq,float eff,float bw,float area,float zin);
+	
+	enum ANT_POL	pol; 	// Antenna Polarization
+	enum ANT_TYPE	type;	// Antenna Type
+	float 		gain; 	// Gain of RX Antenna in dB
+	float 		freq;	// Center Frequency in MHz
+	float 		eff;	// Antenna Efficiency in %
+	float 		bw;		// Antenna Bandwidth in GHz
+	float 		area;	// Effective Aperature Area	
+	float 		zin;	// Impedance in Ohms
+	
+	float antGainFromFreqGHz(float efficiency, float aperature_area, float freqGHz);
+	float calcAeff(float freqGHz);
 		
         float setPol(enum ANT_POL p);   
         float setType(enum ANT_TYPE t);   
@@ -73,12 +72,12 @@ class ANTENNA {
             
         enum ANT_POL getPol() 		{ return pol; }            
         enum ANT_TYPE getType() 	{ return type; }      
-        float getGain() 			{ return gain; }            
-        float getFreq() 			{ return freq; }            
-        float getEff() 				{ return eff; }           
-        float getBW() 				{ return bw; }            
-        float getArea() 			{ return area; }           
-        float getZin() 				{ return zin; }
+        float getGain() 		{ return gain; }            
+        float getFreq() 		{ return freq; }            
+        float getEff() 			{ return eff; }           
+        float getBW() 			{ return bw; }            
+        float getArea() 		{ return area; }           
+        float getZin() 			{ return zin; }
 
 };
 
