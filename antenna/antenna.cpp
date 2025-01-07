9531cf8e685a232222a2c960fa8e759b21ac5cae
jrom876 <jrom876@gmail.com>
2025-01-07 15:36:42 -0700

    Cleanup antenna.cpp

--- antenna/antenna.cpp
+++ antenna/antenna.cpp
@@ -4,9 +4,9 @@
 // 	Purpose:	Define the Antenna Class
 // 	Author:		jrom876
 
-///*************
+///********************
 ///	THE ANTENNA
-///*************
+///********************
 
 /**
  * 	ANTENNA APERATURE EQUATIONS
@@ -49,14 +49,32 @@ using namespace std;
 #include <float.h>	// For FLT_EPSILON
 
 /// STANDARD DEFINITIONS FOR PROJECT SCICALC 
-#define PI	3.14159265358979323846 // ad infinitum
-#define LIGHT_SPEED		299792458 // meters per second
+#define PI			3.14159265358979323846 	// ad infinitum sine repeto
+#define LIGHT_SPEED		299792458.0 		// meters per second
+#define STACK_OVERFLOW		2147483648		// Hex 0x80000000
 #define DATA_SIZE 1000
 #define DELTA 1.0e-6
-#define KILO 1000
-#define MEGA 1000000
-#define GIGA 1000000000
-#define TERA 1000000000000 
+#define MILLI 1.0e-3
+#define MICRO 1.0e-6
+#define NANO 1.0e-9
+#define PICO 1.0e-12
+#define KILO 1.0e3
+#define MEGA 1.0e6
+#define GIGA 1.0e9
+#define TERA 1.0e12 
+#define true 1
+#define false 0
+
+/// STANDARD DEFINITIONS FOR LIGHT INTENSITY AND ELECTRIC FIELD CALCULATIONS
+#define AIR_REFRACTIVE_INDEX 	1.00027717
+#define E0 8.8541878128*PICO				// Permittivity of Free Space in Farads per meter
+#define MU0 1.25663706212*MICRO				// Permeability of Free Space in Newtons per square meter
+#define EPSILON_0 1/(MU0*(LIGHT_SPEED*LIGHT_SPEED))	// Permittivity of Free Space Equation
+#define E_CONSTANT 1/(4*PI*EPSILON_0)
+#define ELECTRON_CHARGE 1.6e-19 			// Charge of an electron in Coulombs
+#define RADIUS_HELIUM_ATOM 26.5e-12			// Radius of a Helium atom in meters
+#define LED_ARRAY_RADIUS 0.35 				// meters from LED array to sample plate
+	
 #define TXT_FILE_LB "linkBudgetData.txt"
 
 #define PLF(deg) pow(cos(deg*(PI/180)),2) 	// Polarization Loss Factor
@@ -79,11 +97,11 @@ ANTENNA::ANTENNA() {
 	pol = horiz; 	// Antenna Polarization
 	type = mono;	// Antenna Type
 	gain = 1.0; 	// Gain of RX Antenna in dB
-	freq = 1.0;		// Center Frequency in MHz
-	eff = 0.0;		// Antenna Efficiency %
-	bw = 1.0;		// Antenna Bandwidth
-	area = 1;		// Effective Aperature Area
-	zin = 50.0;		// Impedance of Antenna
+	freq = 1.0;	// Center Frequency in MHz
+	eff = 0.0;	// Antenna Efficiency %
+	bw = 1.0;	// Antenna Bandwidth
+	area = 1;	// Effective Aperature Area
+	zin = 50.0;	// Impedance of Antenna
 };
 
 // Parameterized Constructor
@@ -131,14 +149,14 @@ float ANTENNA::setZin(float z){zin = z;return zin;};
 int main(int argc, char const *argv[]) {
 	ANTENNA ant1 = ANTENNA();
 	ANTENNA ant2 = ANTENNA(
-					ANTENNA::horiz, // Polarity
-					ANTENNA::mono,	// Type
-					1.0,			// Gain
-					2.4,			// Freq
-					0.9,			// Efficiency
-					1.0,			// Bandwidth
-					1.0,			// Aperature Effective Area
-					50.0);			// Impedance
+			ANTENNA::horiz, // Polarity
+			ANTENNA::mono,	// Type
+			1.0,			// Gain
+			2.4,			// Freq
+			0.9,			// Efficiency
+			1.0,			// Bandwidth
+			1.0,			// Aperature Effective Area
+			50.0);			// Impedance
 	
 	//~ ant1.setPol(ant1.getPol());
 	//~ ant1.setType(ant1.getType());
