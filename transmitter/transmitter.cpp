ebab4e1bca765ef4ad866820d3fa678c2ac58e3d
jrom876 <jrom876@gmail.com>
2025-01-09 08:10:58 -0700

    Update transmitter.cpp

--- transmitter/transmitter.cpp
+++ transmitter/transmitter.cpp
@@ -35,8 +35,8 @@
 using namespace std;
 
 /// STANDARD DEFINITIONS FOR PROJECT SCICALC 
-#define PI		3.14159265358979323846 	// ad infinitum sine repeto
-#define LIGHT_SPEED	299792458.0 		// meters per second
+#define PI	3.14159265358979323846 	// ad infinitum sine repeto
+#define LIGHT_SPEED		299792458.0 		// meters per second
 #define STACK_OVERFLOW	2147483648		// Hex 0x80000000
 #define DATA_SIZE 1000
 #define DELTA 1.0e-6
@@ -52,12 +52,13 @@ using namespace std;
 #define false 0
 
 /// STANDARD DEFINITIONS FOR LIGHT INTENSITY AND ELECTRIC FIELD CALCULATIONS
-#define AIR_REFRACTIVE_INDEX 	1.00027717
+#define AIR_REFRACTIVE_INDEX 1.00027717
 #define E0 8.8541878128*PICO				// Permittivity of Free Space in Farads per meter
 #define MU0 1.25663706212*MICRO				// Permeability of Free Space in Newtons per square meter
 #define EPSILON_0 1/(MU0*(LIGHT_SPEED*LIGHT_SPEED))	// Permittivity of Free Space Equation
-#define E_CONSTANT 1/(4*PI*EPSILON_0)
+#define E_CONSTANT 1/(4*PI*EPSILON_0)			// Permeability of Free Space Equation
 #define ELECTRON_CHARGE 1.6e-19 			// Charge of an electron in Coulombs
+#define RADIUS_HELIUM_ATOM 26.5e-12			// Radius of a Helium atom in meters
 	
 #define TXT_FILE_LB "transmitter_data.txt"
 
