691ea99f50bcfa4bd420c3a1deb7f901f6e01027
jrom876 <jrom876@gmail.com>
2025-01-09 08:12:58 -0700

    Update transmitter.cpp

--- transmitter/transmitter.cpp
+++ transmitter/transmitter.cpp
@@ -35,8 +35,8 @@
 using namespace std;
 
 /// STANDARD DEFINITIONS FOR PROJECT SCICALC 
-#define PI	3.14159265358979323846 	// ad infinitum sine repeto
-#define LIGHT_SPEED		299792458.0 		// meters per second
+#define PI		3.14159265358979323846 	// ad infinitum sine repeto
+#define LIGHT_SPEED	299792458.0 		// meters per second
 #define STACK_OVERFLOW	2147483648		// Hex 0x80000000
 #define DATA_SIZE 1000
 #define DELTA 1.0e-6
@@ -74,6 +74,8 @@ float freqGHz, txpwrW, distTargetKM;
 float radarXSect, antGain;
 //~ float PLF = 0; // Polarization Loss Factor
 float tempf;	// Needed for writing data to file
+
+
 TRANSMITTER::Transmitter DUMMY_TX = {1.0,1.0,0.1,0.1,1};	
 OUTAMP::Outamp DUMMY_OUTAMP = {1.2,250,20,-85,50};
 
