a0d679bd5f737025940de35b4967ceef61b74303
jrom876 <jrom876@gmail.com>
2025-02-07 07:13:11 -0700

    Add files via upload

--- transmitter/attenuator.cpp
+++ transmitter/attenuator.cpp
@@ -7,83 +7,62 @@
 /// C++ Libraries
 #include <cmath>
 #include <cstdio>
-#include <iostream>
 #include <sstream>
+
+#include <bitset>
+#include <iostream>
+#include <cstdint>
+
 #include "attenuator.hpp"
+#include "tools.hpp"
 using namespace std;
-
-/// STANDARD DEFINITIONS FOR PROJECT SCICALC 
-#define PI		3.14159265358979323846 	// ad infinitum sine repeto
-#define LIGHT_SPEED	299792458.0 		// meters per second
-#define STACK_OVERFLOW	2147483648		// Hex 0x80000000
-#define DATA_SIZE 1000
-#define DELTA 1.0e-6
-#define MILLI 1.0e-3
-#define MICRO 1.0e-6
-#define NANO 1.0e-9
-#define PICO 1.0e-12
-#define KILO 1.0e3
-#define MEGA 1.0e6
-#define GIGA 1.0e9
-#define TERA 1.0e12 
-#define true 1
-#define false 0
-
-/// STANDARD DEFINITIONS FOR LIGHT INTENSITY AND ELECTRIC FIELD CALCULATIONS
-#define AIR_REFRACTIVE_INDEX 1.00027717
-#define E0 8.8541878128*PICO				// Permittivity of Free Space in Farads per meter
-#define MU0 1.25663706212*MICRO				// Permeability of Free Space in Newtons per square meter
-#define EPSILON_0 1/(MU0*(LIGHT_SPEED*LIGHT_SPEED))	// Permittivity of Free Space Equation
-#define E_CONSTANT 1/(4*PI*EPSILON_0)
-#define ELECTRON_CHARGE 1.6e-19 			// Charge of an electron in Coulombs
-#define RADIUS_HELIUM_ATOM 26.5e-12			// Radius of a Helium atom in meters
 
 /////////////
 // Globals //
 /////////////
-	
 float tempf;	// Needed for writing data to file
-
-//////////////////
-// Constructors //
-//////////////////
-
 ATTENUATOR::Attenuator DUMMY_ATT = {1,0};
 
 // Default Constructor
 ATTENUATOR::ATTENUATOR() {
-	ATTENUATOR::attenuator	= DUMMY_ATT;
-	ATTENUATOR::freq_in 	= 1; 	// GHz
-	ATTENUATOR::atten 		= 0; 	// dB
+	ATTENUATOR::sig_in 		= 1; 	// GHz
+	ATTENUATOR::atten 		= 0x01; // hex value
+	ATTENUATOR::mask 		= 0xFF; // hex value
+	ATTENUATOR::sig_out 	= (sig_in * GIGA)/(ATTENUATOR::atten & ATTENUATOR::mask); 	// GHz
 };
 
 // DESTRUCTOR
-ATTENUATOR::~ATTENUATOR() {};
+ATTENUATOR::~ATTENUATOR() {std::cout << "Attenuator Destructor is executed\n";};
 
 // Parameterized Constructor
 ATTENUATOR::ATTENUATOR(struct Attenuator att) {
-	ATTENUATOR::attenuator	= att;
-	ATTENUATOR::freq_in 	= att.freq_in;	// 
-	ATTENUATOR::atten 	= att.atten;	// 
+	ATTENUATOR::sig_in	 	= att.sig_in;	// 
+	ATTENUATOR::atten 		= att.atten;	// 
+	ATTENUATOR::mask 		= att.mask;	// 
+	ATTENUATOR::sig_out 	= att.sig_out;	// 
 };
         
-float ATTENUATOR::setFin(float f)	{ freq_in = f; 	return freq_in;	};          
-float ATTENUATOR::setAtten(float a)	{ atten = a;	return atten; };
+void ATTENUATOR::setSig(float s)	{ sig_in = s; };          
+void ATTENUATOR::setAtten(float a)	{ atten = a; };         
+void ATTENUATOR::setMask(float m)	{ mask = m; };
+void ATTENUATOR::setSout(float f)	{ sig_out = f; }; 
 	
 /// Main ///
 int main(int argc, char const *argv[]) {
-	ATTENUATOR::Attenuator testStruct = {1.2,1};
+	ATTENUATOR::Attenuator testStruct = {1.5, 0xFF, 0x04, (testStruct.sig_in/(testStruct.atten & testStruct.mask))};
 	ATTENUATOR t1 = ATTENUATOR();
 	ATTENUATOR t2 = ATTENUATOR(testStruct);
-	t1.setFin(2.1);
-	t1.getFin();
-	t1.setAtten(64);
+	//~ t1.setFin(2.1);
+	t1.getSig();
+	//~ t1.setAtten(64);
 	t1.getAtten();
 	
-	t2.setFin(2.8);
-	t2.getFin();
-	t2.setAtten(128);
+	//~ t2.setFin(2.8);
+	t2.getSig();
+	//~ t2.setAtten(128);
 	t2.getAtten();
+	t2.getMask();
+	t2.getSout();
 	
 	return 0;
 }
--- transmitter/attenuator.hpp
+++ transmitter/attenuator.hpp
@@ -3,15 +3,24 @@
 #ifndef ATTENUATOR_HPP
 #define ATTENUATOR_HPP
 
+#include <bitset>
+#include <iostream>
+#include <cstdint>
+
+
 class ATTENUATOR {
-	private:
-		float 		freq_in; 	// Current freq setting of Attenuator
-		float 		atten; 		// Attenuator setting, 2^7 range (?)	
-		
-	public:		
+    public:		
+    
+		float 		sig_in; 	// Current freq setting of Attenuator
+		uint8_t		atten; 		// output - Attenuator setting, 2^8 range 	
+		uint8_t		mask;		// input - Cutoff Set Point, 2^8 range 	
+		float 		sig_out; 	// output signal (this may become a class)
+				
 		struct Attenuator {
-			float 		freq_in; 	// Current freq setting of Attenuator
-			float 		atten; 		// TX attenuator setting, 2^7 range
+			float 		sig_in; 	// Current freq setting of Attenuator
+			uint8_t		atten; 		// output - Attenuator setting, 2^8 range 	
+			uint8_t		mask;		// input - Cutoff Set Point, 2^8 range 	
+			float 		sig_out; 	// output signal (this may become a class)
 		}attenuator;
 	
 		/// CONSTRUCTORS
@@ -19,15 +28,19 @@ class ATTENUATOR {
 		ATTENUATOR(struct Attenuator attenuator);
 				
 		/// DESTRUCTOR
-		~ATTENUATOR();
+		virtual ~ATTENUATOR();
 		
 		/// SETTERS   
-        float setFin	(float f);           
-        float setAtten	(float a); 
+        void setSig		(float s);           
+        void setAtten	(float a);  
+        void setMask	(float m);           
+        void setSout	(float o); 
         
         /// GETTERS           
-        float getFin() 		{ printf("\nAttenuator freq_in:\t%.2f\n",freq_in); return freq_in; } 
-        float getAtten() 	{ printf("Attenuator atten:\t%.2f\n",atten); return atten; } 
+        float getSig() 		{ printf("\nAttenuator sig_in:\t%.8f\n",sig_in); return sig_in; } 
+        uint8_t getAtten() 	{ printf("Attenuator atten:\t%d\n",atten); return atten; }  
+        uint8_t getMask() 	{ printf("Attenuator mask:\t%d\n",mask); return mask; }         
+        float getSout() 	{ printf("\nAttenuator sig_out:\t%.8f\n",sig_out); return sig_out; } 
        
 };
 
